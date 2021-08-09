17153d051cd17c10f491926037d78459504645cf
KATSIARYNA <statsevich22@gmail.com>
2021-08-09 14:47:13 +0300

    init: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  const nToArr = ('' + n).split('');
+  nToArr.reverse();
+  if (nToArr.includes('-') === true) {
+    nToArr.pop();
+  }
+  let arr = nToArr.join('');
+  let res = Number(arr);
+  return res;
 }
+
